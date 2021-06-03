c4a0c8b6f3900cc762cc40f60911c35304b44b2c
kailaisii <541018378@qq.com>
2021-06-03 10:12:21 +0800

    增加登录功能

--- app/src/main/java/com/kailaisi/hiapp/ui/LoginActivity.kt
+++ app/src/main/java/com/kailaisi/hiapp/ui/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.kailaisi.hiapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kailaisi.common.ui.component.HiBaseActivity
 import com.kailaisi.hiapp.R
+import com.kailaisi.hiapp.databinding.ActivityLoginBinding
+import com.kailaisi.library.util.inflate
 
 class LoginActivity : HiBaseActivity() {
+    private val mBinding:ActivityLoginBinding by inflate()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
