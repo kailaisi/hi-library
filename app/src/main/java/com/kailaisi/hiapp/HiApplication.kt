91c987ea9206adef78cf829d1b34d9a8b4e2784b
wujinxiang <541018378@qq.com>
2021-06-23 17:26:24 +0800

    增加tink对接

--- app/src/main/java/com/kailaisi/hiapp/HiApplication.kt
+++ app/src/main/java/com/kailaisi/hiapp/HiApplication.kt
@@ -1,5 +1,7 @@
 package com.kailaisi.hiapp
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.alibaba.android.arouter.launcher.ARouter
 import com.google.gson.Gson
 import com.kailaisi.common.ui.component.HiBaseApplication
@@ -7,6 +9,8 @@ import com.kailaisi.library.log.HiConsolePrinter
 import com.kailaisi.library.log.HiLogConfig
 import com.kailaisi.library.log.HiLogConfig.JsonParser
 import com.kailaisi.library.log.HiLogManager
+import com.tencent.bugly.Bugly
+import com.tencent.bugly.beta.Beta
 
 /**
  * 描述：
@@ -14,7 +18,7 @@ import com.kailaisi.library.log.HiLogManager
  * 作者：kailaisi
  * <br></br>创建时间：2021-05-10:22:58
  */
-class HIApplication : HiBaseApplication() {
+class HiApplication : HiBaseApplication() {
     override fun onCreate() {
         super.onCreate()
         HiLogManager.init(object : HiLogConfig() {
@@ -35,5 +39,15 @@ class HIApplication : HiBaseApplication() {
             ARouter.openDebug()
         }
         ARouter.init(this)
+        Bugly.init(this, "900029763", false)
+        Bugly.setIsDevelopmentDevice(this,true)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base)
+        // 安装tinker
+        Beta.installTinker()
     }
 }
