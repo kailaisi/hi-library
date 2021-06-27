6bad0bfd8a29b8fdbc84f959396548c6e6533b65
wujinxiang <541018378@qq.com>
2021-06-27 21:44:10 +0800

    增加viewmodel的引入

--- app/src/main/java/com/kailaisi/hiapp/ui/fragment/home/HomeTabFragment.kt
+++ app/src/main/java/com/kailaisi/hiapp/ui/fragment/home/HomeTabFragment.kt
@@ -3,6 +3,8 @@ package com.kailaisi.hiapp.ui.fragment.home
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kailaisi.common.ui.component.HiAbsListFragment
@@ -12,11 +14,13 @@ import com.kailaisi.hiapp.http.api.HomeApi
 import com.kailaisi.hiapp.model.HomeModel
 import com.kailaisi.library.restful.HiCallback
 import com.kailaisi.library.restful.HiResponse
+import com.kailaisi.library.restful.annotion.CacheStrategy
 
 /**
  * 首页的tab对应的Fragment
  */
 class HomeTabFragment : HiAbsListFragment() {
+    private lateinit var viewModel: HomePageViewModel
     private var categoryId: String? = null
     val DEFAULT_TAB_ID = "1"
 
@@ -32,39 +36,33 @@ class HomeTabFragment : HiAbsListFragment() {
 
     override fun onRefresh() {
         super.onRefresh()
-        queryTabTCategoryList()
+        queryTabTCategoryList(CacheStrategy.CACHE_FIRST)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         categoryId = arguments?.getString("id", DEFAULT_TAB_ID)
         super.onViewCreated(view, savedInstanceState)
-        queryTabTCategoryList()
+        viewModel=ViewModelProvider(this)[HomePageViewModel::class.java]
+        queryTabTCategoryList(CacheStrategy.CACHE_FIRST)
         enableLoadMore {
-            queryTabTCategoryList()
+            queryTabTCategoryList(CacheStrategy.NET_ONLY)
         }
     }
+
 
     override fun createLayoutManager(): RecyclerView.LayoutManager {
         var hot = TextUtils.equals(categoryId, DEFAULT_TAB_ID)
         return if (hot) super.createLayoutManager() else GridLayoutManager(context, 2)
     }
 
-    private fun queryTabTCategoryList() {
-        ApiFactory.create(HomeApi::class.java).queryTabCategoryList(categoryId!!)
-            .enqueue(object : HiCallback<HomeModel> {
-                override fun onSuccess(response: HiResponse<HomeModel>) {
-                    if (response.successful() && response.data != null) {
-                        updateUi(response.data!!)
-                    } else {
-                        finishRefresh(null)
-                    }
-                }
-
-                override fun onFailed(throwable: Throwable) {
-                    finishRefresh(null)
-                }
-
-            })
+    private fun queryTabTCategoryList(cacheStrategy: Int) {
+        viewModel.queryTabCategoryList(categoryId,pageIndex,cacheStrategy).observe(viewLifecycleOwner, Observer {
+            if (it!=null){
+                updateUi(it)
+            }else{
+                finishRefresh(null)
+            }
+        })
     }
 
     private fun updateUi(data: HomeModel) {
